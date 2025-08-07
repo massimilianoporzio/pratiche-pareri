9af630609cd9dfe3d88ce676b93d626d4cf557ff
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-08-07 18:25:31 +0200

    sistema di logging impostato

--- pratiche/urls.py
+++ pratiche/urls.py
@@ -1,7 +1,16 @@
+# import logging
+
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
+
+# logger = logging.getLogger(__name__)
+# logger.debug("This is a debug message")
+# logger.info("This is an info message")
+# logger.warning("This is a warning message")
+# logger.error("This is an error message")
+# logger.critical("This is a critical message")
 
 urlpatterns = [
     path("admin/", admin.site.urls),
