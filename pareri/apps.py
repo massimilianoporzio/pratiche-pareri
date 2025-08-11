33daa68fdc039295c85948522aa5c77758535076
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-08-11 17:36:30 +0200

    rieseguito pre-commit per formattare / correggere

--- pareri/apps.py
+++ pareri/apps.py
@@ -2,5 +2,5 @@ from django.apps import AppConfig
 
 
 class PareriConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'pareri'
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "pareri"
