a8461c1c156555f28b8c8f0dca00f276384cbdb0
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-08-13 16:25:09 +0200

    varie per datore di lavoro e sedi

--- pareri/admin.py
+++ pareri/admin.py
@@ -1,18 +1,20 @@
 from django.contrib import admin
 
-from pareri.models import EspertoRadioprotezione, TipoPratica, TipoProcesso
+from pareri.models import EspertoRadioprotezione, TipoOrigine, TipoPratica, TipoProcesso
 from pratiche.admin import ActiveModelAdminMixin, custom_admin_site
 
 
 @admin.register(TipoProcesso, site=custom_admin_site)
-class TipoProcessoAdmin(admin.ModelAdmin):
-    fields = ("nome", "descrizione")
+class TipoProcessoAdmin(admin.ModelAdmin, ActiveModelAdminMixin):
+    list_filter = ("is_active",)
+    fields = ("nome", "descrizione", "is_active")
     list_display = ("nome", "descrizione")
 
 
 @admin.register(EspertoRadioprotezione, site=custom_admin_site)
-class EspertoRadioprotezioneAdmin(admin.ModelAdmin):
-    fields = ("numero_iscrizione", "full_name")
+class EspertoRadioprotezioneAdmin(admin.ModelAdmin, ActiveModelAdminMixin):
+    list_filter = ("is_active",)
+    fields = ("numero_iscrizione", "full_name", "is_active")
     list_display = ("numero_iscrizione", "full_name")
     search_fields = ("numero_iscrizione", "full_name")
 
@@ -25,3 +27,9 @@ class TipoPraticaAdmin(admin.ModelAdmin, ActiveModelAdminMixin):
         "nome",
         "descrizione",
     )
+
+
+@admin.register(TipoOrigine, site=custom_admin_site)
+class TipoOrigineAdmin(admin.ModelAdmin, ActiveModelAdminMixin):
+    list_filter = ("is_active",)
+    fields = ("nome", " is_active")
