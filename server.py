01bd11dd7b84536d9fa0807ad5498fa4f0449008
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-08-20 15:59:37 +0200

    waitress serve

--- server.py
+++ server.py
@@ -1,6 +1,6 @@
 from waitress import serve
 
-from pratiche.wsgi import application
+from pratiche.wsgi_prod import application
 
 if __name__ == "__main__":
     serve(application, host="0.0.0.0", port=8080)
