90e501d5f10de37cbf3e0fd14bcd698375f2dc39
Massimiliano Porzio <massimiliano.porzio@gmail.com>
2025-08-20 11:47:06 +0200

    messo altri staticfiles con collectstatic

---
+++ staticfiles/js/toggle_checkboxes.js
@@ -0,0 +1,24 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const formsetTable = document.getElementById('datorelavorosede_set-group');
+
+    if (formsetTable) {
+        formsetTable.addEventListener('change', function(event) {
+            const clickedCheckbox = event.target;
+
+            // Assicurati che l'elemento cliccato sia una checkbox "is_sede_legale"
+            if (clickedCheckbox.name.includes('is_sede_legale') && clickedCheckbox.type === 'checkbox') {
+                if (clickedCheckbox.checked) {
+                    // Seleziona solo le checkbox all'interno del formset corrente
+                    const allCheckboxes = formsetTable.querySelectorAll('input[name*="is_sede_legale"]');
+
+                    allCheckboxes.forEach(checkbox => {
+                        // Deseleziona le altre checkbox
+                        if (checkbox !== clickedCheckbox) {
+                            checkbox.checked = false;
+                        }
+                    });
+                }
+            }
+        });
+    }
+});
